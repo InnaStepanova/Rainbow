f98e044d2ea5c7e004c809af0edbdc28e5320a54
InnaStepanova <inna-panichkina@yandex.ru>
2023-11-13 12:32:36 +0300

    Initial Commit

---
+++ Rainbow/RainbowApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainbowApp.swift
+//  Rainbow
+//
+//  Created by Лаванда on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct RainbowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
