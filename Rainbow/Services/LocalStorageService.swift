1bbdeba4fdb309906a679774adee5b98c2746b15
Alexandr Rodionov <alexrodionby@gmail.com>
2023-11-14 22:36:48 +0300

    added a save service to the local storage

---
+++ Rainbow/Services/LocalStorageService.swift
@@ -0,0 +1,46 @@
+//
+//  LocalStorageService.swift
+//  Rainbow
+//
+//  Created by Alexandr Rodionov on 14.11.23.
+//
+
+import Foundation
+
+final class LocalStorageService {
+    
+    private init() {}
+    static let shared = LocalStorageService()
+    
+    
+    func saveSettings(settings: SettingsModel, name: String) {
+        save(settings, key: name)
+    }
+    
+    func loadSettings(settingsName: String) -> SettingsModel? {
+        load(key: settingsName)
+    }
+    
+    // MARK: - private save / load methods
+    
+    private func save<T: Codable>(_ object: T, key: String) {
+        do {
+            let data = try JSONEncoder().encode(object)
+            UserDefaults.standard.set(data, forKey: key)
+        } catch {
+            print("\(T.Type.self) saving failed")
+        }
+    }
+    
+    private func load<T: Codable>(key: String) -> T? {
+        guard let data = UserDefaults.standard.data(forKey: key) else { return nil }
+        
+        do {
+            let object = try JSONDecoder().decode(T.self, from: data)
+            return object
+        } catch {
+            print("\(T.Type.self) loading failed")
+            return nil
+        }
+    }
+}
