c35484a09702346fd1fb0b924065adee3e7e3067
Alex Zhulai <Lepiozdyx@gmail.com>
2023-11-14 17:52:05 -0300

    Add GeometryReader to MainMenuView + small changes in RainbowApp(add EnvironmentObject).

--- Rainbow/Screens/MainMenu/MainMenuView.swift
+++ Rainbow/Screens/MainMenu/MainMenuView.swift
@@ -8,61 +8,61 @@
 import SwiftUI
 
 struct MainMenuView: View {
+    @EnvironmentObject var viewModel: GameEngineViewModel
     
     var body: some View {
         NavigationView {
-            ZStack {
-                Color.gray.ignoresSafeArea()
-                
-                VStack(spacing: 20) {
-                    Spacer()
+            GeometryReader { geometry in
+                ZStack {
+                    Color.gray.ignoresSafeArea()
                     
-                    Image(.rainbow)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: .infinity)
-                        .shadow(radius: 10)
-                    
-                    Text("НЛП игра")
-                        .font(.system(size: 36))
-                    
-                    Text("Радуга")
-                        .font(.system(size: 64, weight: .light, design: .serif))
-                        .shadow(color: .black.opacity(0.5), radius: 4, x: 0, y: 4)
-                    
-                    Spacer()
-                    
-                    VStack(spacing: 16) {
-                        NavigationLink(destination: Text("NEW GAME VIEW")) {
-                            CustomButtonView(title: "Новая игра", color: .customPink)
-                        }
+                    VStack(spacing: 20) {
+                        Image(.rainbow)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: geometry.size.width * 0.8)
+                            .shadow(radius: 10)
                         
-                        NavigationLink(destination: Text("CONTINUE PLAYING VIEW")) {
-                            CustomButtonView(title: "Продолжить", color: .customBlue)
-                        }
+                        Text("НЛП игра")
+                            .font(.system(size: geometry.size.width * 0.1))
                         
-                        NavigationLink(destination: Text("STATISTICS VIEW")) {
-                            CustomButtonView(title: "Статистика", color: .customGreen)
-                        }
-                    }
-                    
-                    Spacer()
-                    
-                    HStack {
-                        NavigationLink(destination: Text("SETTINGS VIEW")) {
-                            CustomIconButtonView(name: "gearshape.fill")
-                        }
+                        Text("Радуга")
+                            .font(.system(size: geometry.size.width * 0.17, weight: .light, design: .serif))
+                            .shadow(color: .black.opacity(0.5), radius: 4, x: 0, y: 4)
                         
                         Spacer()
                         
-                        NavigationLink(destination: RulesMainView()) {
-                            CustomIconButtonView(name: "questionmark")
+                        VStack(spacing: 16) {
+                            NavigationLink(destination: TagUIView()) {
+                                CustomButtonView(title: "Новая игра", color: .customPink)
+                            }
+                            
+                            NavigationLink(destination: Text("CONTINUE PLAYING VIEW")) {
+                                CustomButtonView(title: "Продолжить", color: .customBlue)
+                            }
+                            
+                            NavigationLink(destination: Text("STATISTICS VIEW")) {
+                                CustomButtonView(title: "Статистика", color: .customGreen)
+                            }
                         }
+                        
+                        HStack {
+                            NavigationLink(destination: Text("SETTINGS VIEW")) {
+                                CustomIconButtonView(name: "gearshape.fill")
+                            }
+                            
+                            Spacer()
+                            
+                            NavigationLink(destination: RulesMainView()) {
+                                CustomIconButtonView(name: "questionmark")
+                            }
+                        }
+                        .padding()
                     }
-                    .padding()
+                    .preferredColorScheme(.light)
                 }
-                .preferredColorScheme(.light)
             }
+            
         }
     }
 }
