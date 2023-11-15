bda19bd1bc8e6aeb4e7a4c8d3df190ac8c1989e1
Alex <y10b@ya.ru>
2023-11-15 21:20:45 +0100

    GameEngine, UI
    - pause
    - random
    - tag View
    - settings
    - model

---
+++ Rainbow/Extensions/TextForTagView+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  TextForTagView+Ext.swift
+//  Rainbow
+//
+//  Created by Alex on 15.11.2023.
+//
+
+import Foundation
+
+enum TextForTagView: String, CaseIterable {
+    case red = "Красный"
+    case orange = "Оранжевый"
+    case yellow = "Желтый"
+    case green = "Зеленый"
+    case blue = "Синий"
+    case indigo = "Индиго"
+    case violet = "Фиолетовый"
+}
--- Rainbow/Models/SettingsModel.swift
+++ Rainbow/Models/SettingsModel.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 struct SettingsModel: Codable {
-    let minutesSlider: Double
+    let gameDuration: Double
     let speedOfChangingWords: Double
-    let backgroundForText: Bool
+    let isBackgroundForView: Bool
+    let isChangeTextColor: Bool
 }
